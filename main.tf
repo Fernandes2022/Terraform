0c4b707604303571d80f1855c3cf4023b450a734
Fernandes2022 <99113572+Fernandes2022@users.noreply.github.com>
2026-07-18 16:42:22 +0100

    Comment out TFE outputs data source
    Comment out data source for TFE outputs in main.tf

--- main.tf
+++ main.tf
@@ -31,11 +31,12 @@ module "vpc" {
   enable_dns_hostnames = true
 }
 
-
+/*
 data "tfe_outputs" "source_workspace" {
   workspace    = var.workspace_name
   organization = var.organization_name
 }
+*/
 
 
 
